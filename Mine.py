110615bd6fc6279dd39f022163ff9a3ce8836986
LanXuan <lanx8@chinaunicom.cn>
2019-10-30 17:01:39 +0800

    lx-1.本地环skleran分类测试

--- Mine.py
+++ Mine.py
@@ -1,1 +1,5 @@
-import  sklearn;
+import pandas as pd
+
+csv_data = pd.read_csv("C:\\Users\\为了熊\Desktop\\工作\\数据挖掘\\xhzw_OUTPUT_0051_19050607.csv")  # 读取训练数据
+newD = csv_data.loc[0:20000]
+newD.to_csv("demo.csv")
---
+++ test.py
@@ -0,0 +1,9 @@
+j = 6
+if j < 2:
+    print("1")
+elif j == 5:
+    print("12")
+elif j < 28:
+    print("13")
+else:
+    print("133")
